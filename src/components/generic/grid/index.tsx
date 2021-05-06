ca7fddb6d0fbb8af150142c32745c0e5735e586b
ali <a.khadim@xpay.de>
2021-05-06 16:53:57 +0200

    Updated the Grid component add more props

--- src/components/generic/grid/index.tsx
+++ src/components/generic/grid/index.tsx
@@ -1,14 +1,27 @@
 import styles from './grid.module.css'
-
+import clsx from 'clsx'
+type Breakpoints = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
 interface Props {
-  grid: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
   children: React.ReactNode
   className?: string
+  xs?: Breakpoints
+  sm?: Breakpoints
+  md?: Breakpoints
+  lg?: Breakpoints
+  xl?: Breakpoints
 }
 
-const Grid: React.FC<Props> = ({ children, grid, ...props }: Props) => {
+const Grid: React.FC<Props> = ({ children, xs, sm, md, lg, xl, ...props }: Props) => {
   return (
-    <div className={styles['col-' + grid]} {...props}>
+    <div
+      className={clsx(
+        styles[`col-xs-${xs}`],
+        styles[`col-sm-${sm}`],
+        styles[`col-md-${md}`],
+        styles[`col-lg-${lg}`],
+        styles[`col-xl-${xl}`]
+      )}
+      {...props}>
       {children}
     </div>
   )
